278b4d75fb10e5a30b5401c88f1fbaaa3de6db6a
Rommel Soriano <rommelsoriano454@gmail.com>
2021-09-05 23:49:59 -0500

    Autorizacion de creacion de usuarios por el administrador y cancelar usuarios repetidos.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,33 @@
-export const createUser = (req, res) => {
-    res.json('creating user')
-}
+import User from "../models/user";
+import Role from "../models/role";
+
+export const createUser = async (req, res) => {
+  try {
+    const { username, email, password, roles } = req.body;
+
+    const rolesFound = await Role.find({ name: { $in: roles } });
+
+    // creating a new User
+    const user = new User({
+      username,
+      email,
+      password,
+      roles: rolesFound.map((role) => role._id),
+    });
+
+    // encrypting password
+    user.password = await User.encryptPassword(user.password);
+
+    // saving the new user
+    const savedUser = await user.save();
+
+    return res.status(200).json({
+      _id: savedUser._id,
+      username: savedUser.username,
+      email: savedUser.email,
+      roles: savedUser.roles,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
