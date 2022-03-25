1563e54564724dc553f43a9a376889d57b5a7ab1
Rommel Soriano <rommelsoriano454@gmail.com>
2022-03-25 13:56:41 -0500

    Update authJwt.js
    Se actualizo el authJwt.js

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -7,7 +7,7 @@ export const verifyToken = async (req, res, next) => {
   try {
     const token = req.headers["x-access-token"];
 
-    if (!token) return res.status(403).json({ message: "No token provided." });
+    if (!token) return res.status(403).json({ message: "No token privided." });
 
     const decoded = jwt.verify(token, config.SECRET);
     req.userId = decoded.id;
